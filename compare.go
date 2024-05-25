6bb59cb148f30a8b0c7ff8c046b05d12f36c8fe9
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-05-25 10:26:59 +0900

    add CompareNotEqual

--- compare.go
+++ compare.go
@@ -14,3 +14,13 @@ func Compare(t *testing.T, actual interface{}, expected interface{}) {
 			f.Name(), file, line)
 	}
 }
+
+func CompareNotEqual(t *testing.T, actual interface{}, expected interface{}) {
+	pc, file, line, _ := runtime.Caller(1)
+	f := runtime.FuncForPC(pc)
+
+	if actual == expected {
+		t.Errorf("got: %v, %T\nnot want: %v, %T\ncalled from %s on %s:%d", actual, actual, expected, expected,
+			f.Name(), file, line)
+	}
+}
--- test/compare_test.go
+++ test/compare_test.go
@@ -13,3 +13,11 @@ func Test_01(t *testing.T) {
 func Test_02(t *testing.T) {
 	cp.Compare(t, string("kaeru"), string("🐸"))
 }
+
+func Test_03(t *testing.T) {
+	cp.CompareNotEqual(t, string("kaeru"), string("kaeru"))
+}
+
+func Test_04(t *testing.T) {
+	cp.CompareNotEqual(t, string("kaeru"), string("🐸"))
+}
